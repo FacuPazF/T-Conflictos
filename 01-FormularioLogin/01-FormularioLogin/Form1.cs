7da63cb4e014360131f77965ec229a83b6ab4f6f
acosta0xf <acosta0xf@gmail.com>
2020-08-06 19:25:41 -0300

    Carga del Proyecto Original

--- 01-FormularioLogin/01-FormularioLogin/Form1.cs
+++ 01-FormularioLogin/01-FormularioLogin/Form1.cs
@@ -10,7 +10,6 @@ namespace _01_FormularioLogin
             InitializeComponent();
         }
 
-        // Método para el evento del botón Ingresar
         private void btnIngresar_Click(object sender, EventArgs e)
         {
             string usuario = "admin";
